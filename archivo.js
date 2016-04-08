eee94e1f8102db5f00bd791b39f335806d6cf021
Rodrigo <yigo2227@gmail.com>
2016-04-07 22:06:31 -0500

    v6

--- archivo.js
+++ archivo.js
@@ -1,6 +1,6 @@
 
 var iframe1 = '<iframe width="600" height="450" frameborder="0" style="border:0"
-src="https://www.google.com/maps/embed/v1/search?q=Hoteles+en+Oaxaca&key=AIzaSyAU7_l7egJjMuRDkwwFf5v3V8gSKofP0cw" allowfullscreen></iframe>';
+src="https://www.google.com/maps/embed/v1/search?q=Bares+en+Oaxaca&key=AIzaSyAU7_l7egJjMuRDkwwFf5v3V8gSKofP0cw" allowfullscreen></iframe>';
  
 //Funciones que identifican el lugar en la página y escriben el valor de la variable. 
  
